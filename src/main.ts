eec23d45eb1a561fbb3132a3029b0de4a10ebdad
Tolotra <tolotor@yahoo.fr>
2023-04-05 22:26:30 +0300

    Global setup for validation pipes and refactoring in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { useContainer } from 'class-validator';
+
+import { formatValidationErrors } from './common/helpers/formatters.helper';
 
 async function bootstrap() {
+  // The base application
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes();
+
+  // CORS handling
+  app.enableCors();
+
+  // Global pipe for the DTO validations
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory(errors) {
+        // Errors formatting
+        const formattedErrors = formatValidationErrors(errors);
+        return new BadRequestException(formattedErrors);
+      },
+    }),
+  );
+  // Containerizing the DTO validations to the scope of the App module
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  // PORT setup
   await app.listen(5000);
 }
 bootstrap();
