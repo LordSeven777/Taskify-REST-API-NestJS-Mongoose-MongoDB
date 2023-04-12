7c6f3eb51b0152c71f76f8af7e81bd202c36932b
Tolotra <tolotor@yahoo.fr>
2023-04-12 22:24:41 +0300

    Set config modue as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+import { AppController } from './app.controller';
+import { AuthModule } from './api/auth/auth.module';
+import { UserModule } from './api/user/user.module';
+import { TaskModule } from './api/task/task.module';
+import { LabelModule } from './api/label/label.module';
+
+@Module({
+  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL as string),
+    AuthModule,
+    UserModule,
+    TaskModule,
+    LabelModule,
+  ],
+})
 export class AppModule {}
