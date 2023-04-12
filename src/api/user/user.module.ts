1c878d10e78636bdbd0575630d57ca8e3876efac
Tolotra <tolotor@yahoo.fr>
2023-04-12 22:22:15 +0300

    Export user mongoose module

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { UniqueUserFieldValidator } from './decorator/unique-field.decorator';
 
+const userMongooseModule = MongooseModule.forFeature([
+  { name: User.name, schema: UserSchema },
+]);
+
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [userMongooseModule],
   providers: [UniqueUserFieldValidator],
-  exports: [UniqueUserFieldValidator],
+  exports: [UniqueUserFieldValidator, userMongooseModule],
 })
 export class UserModule {}
