f4d59cd5fab8014fa9a76dea571db59a90f4a97b
Tolotra <tolotor@yahoo.fr>
2023-04-13 16:02:07 +0300

    Access token and Refresh token JWT strategies

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { AccessTokenJwtStrategy, RefreshTokenJwtStrategy } from './strategy';
 
 @Module({
   imports: [UserModule, JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenJwtStrategy, RefreshTokenJwtStrategy],
 })
 export class AuthModule {}
