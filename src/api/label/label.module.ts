56338a84cfcc56527b7582334d6a5053a9677be0
Tolotra <tolotor@yahoo.fr>
2023-04-14 18:18:04 +0300

    Import of label module into user module

--- src/api/label/label.module.ts
+++ src/api/label/label.module.ts
@@ -3,9 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { Label, LabelSchema } from './label.schema';
 
+const labelMongooseModule = MongooseModule.forFeature([
+  { name: Label.name, schema: LabelSchema },
+]);
+
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Label.name, schema: LabelSchema }]),
-  ],
+  imports: [labelMongooseModule],
+  exports: [labelMongooseModule],
 })
 export class LabelModule {}
--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -6,13 +6,14 @@ import { UniqueUserFieldValidator } from './decorator/unique-field.decorator';
 import { UserService } from './user.service';
 import { TaskModule } from '../task/task.module';
 import { UserController } from './user.controller';
+import { LabelModule } from '../label/label.module';
 
 const userMongooseModule = MongooseModule.forFeature([
   { name: User.name, schema: UserSchema },
 ]);
 
 @Module({
-  imports: [userMongooseModule, TaskModule],
+  imports: [userMongooseModule, TaskModule, LabelModule],
   providers: [UniqueUserFieldValidator, UserService],
   exports: [UniqueUserFieldValidator, userMongooseModule, UserService],
   controllers: [UserController],
