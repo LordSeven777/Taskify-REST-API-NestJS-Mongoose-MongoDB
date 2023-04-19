afd607a2bb5cb56d572efa7e2f5fe9ecc451e90f
Tolotra <tolotor@yahoo.fr>
2023-04-19 12:26:02 +0300

    Task controller: Get one

--- src/api/task/task.controller.ts
+++ src/api/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 
 import { TaskService } from './task.service';
 import { AccessTokenGuard, PolicyGuard } from '../auth/guards';
@@ -7,6 +7,8 @@ import { TaskPolicy } from './task.policy';
 import { UserDocument } from '../user/user.schema';
 import { UserAction } from '../auth/auth.policy';
 import { AuthUser, CheckPolicy } from '../auth/decorator';
+import { TaskDocument } from './task.schema';
+import { BindTaskParamPipe } from './pipe';
 
 @Controller('api/tasks')
 export class TaskController {
@@ -14,6 +16,16 @@ export class TaskController {
     private taskService: TaskService,
     private taskPolicy: TaskPolicy,
   ) {}
+
+  @Get(':id')
+  @UseGuards(AccessTokenGuard)
+  async getOne(
+    @Param('id', BindTaskParamPipe) task: TaskDocument,
+    @AuthUser() authUser: UserDocument,
+  ) {
+    this.taskPolicy.authorize(authUser, UserAction.Read, task);
+    return task;
+  }
 
   @Post('')
   @UseGuards(AccessTokenGuard)
